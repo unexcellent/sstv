6d8885f33f6c28a208ee3666fafe2822a0d1832a
unexcellent <>
2026-04-05 14:57:18 +0200

    test: move robot36 specific tests

--- src/modes/robot36.rs
+++ src/modes/robot36.rs
@@ -7,3 +7,59 @@ impl Mode for Robot36 {
     const IMAGE_WIDTH: u16 = 320;
     const IMAGE_HEIGHT: u16 = 240;
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::modes::robot36::Robot36;
+    use crate::synthesizer::Tone;
+    use crate::units::{Duration, Frequency};
+    use crate::{Hz, ms};
+
+    #[test]
+    fn test_identification_tones_robot36() {
+        assert_eq!(
+            Robot36.identification_sequence(),
+            [
+                Tone(Robot36::BINARY_0, Robot36::BIT_DURATION),
+                Tone(Robot36::BINARY_0, Robot36::BIT_DURATION),
+                Tone(Robot36::BINARY_0, Robot36::BIT_DURATION),
+                Tone(Robot36::BINARY_1, Robot36::BIT_DURATION),
+                Tone(Robot36::BINARY_0, Robot36::BIT_DURATION),
+                Tone(Robot36::BINARY_0, Robot36::BIT_DURATION),
+                Tone(Robot36::BINARY_0, Robot36::BIT_DURATION),
+                Tone(Robot36::BINARY_1, Robot36::BIT_DURATION),
+            ]
+        )
+    }
+
+    #[test]
+    fn test_header_tones_robot36() {
+        assert_eq!(
+            Robot36.header_sequence(),
+            [
+                Tone(Hz!(1900), ms!(100)),
+                Tone(Hz!(1500), ms!(100)),
+                Tone(Hz!(1900), ms!(100)),
+                Tone(Hz!(1500), ms!(100)),
+                Tone(Hz!(2300), ms!(100)),
+                Tone(Hz!(1500), ms!(100)),
+                Tone(Hz!(2300), ms!(100)),
+                Tone(Hz!(1500), ms!(100)),
+                Tone(Hz!(1900), ms!(300)),
+                Tone(Hz!(1200), ms!(10)),
+                Tone(Hz!(1900), ms!(300)),
+                Tone(Hz!(1200), ms!(30)),
+                Tone(Hz!(1300), ms!(30)),
+                Tone(Hz!(1300), ms!(30)),
+                Tone(Hz!(1300), ms!(30)),
+                Tone(Hz!(1100), ms!(30)),
+                Tone(Hz!(1300), ms!(30)),
+                Tone(Hz!(1300), ms!(30)),
+                Tone(Hz!(1300), ms!(30)),
+                Tone(Hz!(1100), ms!(30)),
+                Tone(Hz!(1200), ms!(30)),
+            ]
+        )
+    }
+}
