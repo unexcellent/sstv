0e863d7acaf2a139f16e9e3d617f6367033b53f9
unexcellent <>
2026-04-16 16:27:40 +0200

    docs: document encoder

--- src/encoder.rs
+++ src/encoder.rs
@@ -5,11 +5,24 @@ use crate::image::RgbPixel;
 use crate::modes::Mode;
 use crate::synthesizer::Tone;
 
+/// `Encoder` is the main struct for converting an image into SSTV tones.
+///
+/// Construct `Encoder` with your desired mode and an iterator over the pixels you want to encode.
+/// ```rust
+/// use sstv::{Encoder, Error, Mode, RgbPixel};
+///
+/// let image = [RgbPixel::new(0, 0, 0); 320];
+/// let encoder = Encoder::new(Mode::Robot36, image.into_iter());
+/// for tone in encoder {
+///     // emit or save the tones
+/// }
+/// ```
 pub struct Encoder {
     inner: Box<dyn Iterator<Item = Tone>>,
 }
 
 impl Encoder {
+    /// Construct an `Encoder` from the mode and a pixel iterator.
     pub fn new<I>(mode: Mode, pixels: I) -> Result<Self>
     where
         I: Iterator<Item = RgbPixel> + 'static,
