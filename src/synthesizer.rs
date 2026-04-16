52449be8fccd032cebb68c00705f1a5b92e4ddc3
unexcellent <>
2026-04-16 16:51:36 +0200

    docs: document tone

--- src/synthesizer.rs
+++ src/synthesizer.rs
@@ -1,4 +1,5 @@
 use crate::units::{Duration, Frequency};
 
 #[derive(Debug, Clone, Copy, PartialEq)]
+/// A single frequency emitted for a certain duration.
 pub struct Tone(pub Frequency, pub Duration);
